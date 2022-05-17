0f9e9854f0358fed6551fbf11b1e1cf4d37d7c58
Alejandro Lima <73153439+Drojann@users.noreply.github.com>
2022-05-16 22:03:04 -0500

    sprint3

---
+++ app/views/UserView.js
@@ -0,0 +1,16 @@
+const UserService = require('./../services/UserService')
+
+class UserView {
+    static createUser (payload){
+        if (payload === null){
+        return {error: "payload no existe"}}
+        else if ((typeof payload.username !=='string'||payload.username === null)||
+                (typeof payload.name !=='string'||payload.name === null)||
+                (typeof payload.id !=='number'||payload.id === null)){
+                    return {error: "necesitan tener un valor valido"}}
+            else {
+                    return UserService.create(payload.id, payload.username, payload.name)
+                    }
+        }}
+
+module.exports = UserView
